56f85840a67b362a09c5d551ffad417af2dee054
aaryan2807 <aryan.khanna@chicmic.co.in>
2023-01-30 10:48:05 +0530

    First Commit

---
+++ async-await.ts
@@ -0,0 +1,29 @@
+import { timeStamp } from "console";
+import fetch from 'node-fetch';
+const word = "https://api.agify.io?name=bella";
+// function setup(){
+//     temp()
+//     .then(results => console.log(`I am in promise ${results.name}`))
+//     .catch(err =>console.log(err))
+    
+// }
+function setup(){
+    temp()
+    .then(results => {
+        console.log(`I am in promise ${results.name}`)
+        return temp()
+    })
+    .then(results => console.log(`i am in second then ${results.name}`))
+    .catch(err =>console.log(err))
+    
+}
+async function temp() {
+    let response1=await fetch(word)
+    let json1=await response1.json()
+    return{
+        name:json1.name
+    }
+    
+    
+}
+setup()
---
+++ promises.ts
@@ -0,0 +1,53 @@
+import { createHash } from 'crypto';
+import fetch from 'node-fetch';
+import crypto from 'crypto';
+import { json } from 'stream/consumers';
+import { rejects } from 'assert';
+const word = "https://api.agify.io?name=bella";
+function setup() {
+
+//SIMPLE PROMISE
+
+//   let promise = fetch(word);
+//   promise.then(gotdata);
+//   promise.catch(goterror);
+
+//   function gotdata(data: any) {
+//     var data= data.json();
+//     var json=json.name
+//     var cryp=crypto.createHash('sha256',json)
+//     .digest('hex')
+//     console.log(cryp)
+
+//   }
+//   function goterror(err: any) {
+//     console.log(err);
+//   }
+
+// fetch(word)
+// .then((data)=> console.log(data))
+// .catch((err)=> console.log(err))
+// fetch(word)
+// .then(data=>data.json())
+// .then(json=>console.log(json.name))
+// .catch(err=> console.log(err))
+
+
+//USER DEFINED PROMISE
+delay(1000)
+.then(data=>console.log('hello'))
+.catch(err=>console.log(err))
+}
+function delay(time:any){
+    return new Promise((resolve,reject)=>{
+        if(isNaN(time)){
+            reject(console.log("not right"))
+        }
+        setTimeout(resolve,time)
+    })
+
+
+
+
+}
+setup();
